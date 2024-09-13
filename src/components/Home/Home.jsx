025317df135c1a2191c3a42573ac614e907f5c01
labcabrera <lab.cabrera@gmail.com>
2024-09-13 16:59:17 +0200

    Initial version.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="homepage">
-      <h2>Rolemaster host wip</h2>
-    </div>
+    <div>
+      <div>
+        This application provides a set of services for playing Rolemaster Unified developed by https://ironcrown.com/.
+      </div>
+      <div>
+        It consists of the following modules:
+      </div>
+      <p>
+        <ul>
+          <li><a routerLink="/characters">Character generation</a>: allows the creation of characters, their customization and leveling up.</li>
+          <li><a routerLink="/strategic-sessions">Strategic sessions</a>: equivalent to what would be a campaign within which there can be different tactical
+            sessions.</li>
+          <li><a routerLink="/tactical-sessions">Tactical sessions</a>: execution of actions that generally involve combat or maneuver situations.</li>
+        </ul>
+      </p>
+
+      Under development.
+
+    </div >
   );
 };
 
