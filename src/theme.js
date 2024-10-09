d6e24f78675535a2552ec68a53d7190f14706de5
labcabrera <lab.cabrera@gmail.com>
2024-10-09 17:36:13 +0200

    Updated styles.

--- src/theme.js
+++ src/theme.js
@@ -1,27 +1,26 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-    palette: {
-        mode: 'dark'
-    },
-    typography: {
-        fontFamily: 'ringbearer, sans-serif',
-    },
-    components: {
-        MuiCssBaseline: {
-            styleOverrides: `
+  palette: {
+    mode: 'dark'
+  },
+  typography: {
+    fontFamily: 'ringbearer, sans-serif',
+    fontSize: 20,
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
         @font-face {
           font-family: 'ringbearer';
-          //TODO review webpack config
-          //src: url('./assets/fonts/ringbearer.ttf') format('truetype');
           src: url('/static/fonts/ringbearer.ttf') format('truetype');
         }
         body {
           font-family: 'ringbearer', sans-serif;
         }
       `,
-        },
     },
+  },
 });
 
 export default theme;
