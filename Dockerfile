566ce6aebe809ee54341ffb63905e7f285bad791
labcabrera <lab.cabrera@gmail.com>
2025-07-14 00:47:55 +0200

    Updated dev config.

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,22 @@
-FROM node:24 AS builder
-WORKDIR /app
+FROM node:24
+WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-RUN npm run build
+EXPOSE 8080
+CMD ["npm", "start"]
 
-FROM nginx:alpine
-COPY --from=builder /app/dist /usr/share/nginx/html
-COPY docker-entrypoint.sh /docker-entrypoint.sh
-RUN chmod +x /docker-entrypoint.sh
+# FROM node:24 AS builder
+# WORKDIR /app
+# COPY package*.json ./
+# RUN npm install
+# COPY . .
+# RUN npm run build
 
-ENTRYPOINT ["/docker-entrypoint.sh"]
-CMD ["nginx", "-g", "daemon off;"]
+# FROM nginx:alpine
+# COPY --from=builder /app/dist /usr/share/nginx/html
+# COPY docker-entrypoint.sh /docker-entrypoint.sh
+# RUN chmod +x /docker-entrypoint.sh
+
+# ENTRYPOINT ["/docker-entrypoint.sh"]
+# CMD ["nginx", "-g", "daemon off;"]
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,9 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (_, argv) => ({
   output: {
-    publicPath: process.env.RMU_FE_HOST_PUBLIC_PATH || "http://localhost:8080/",
+    //publicPath: process.env.RMU_FE_HOST_PUBLIC_PATH || "http://localhost:8080/",
+    //publicPath: process.env.RMU_FE_HOST_PUBLIC_PATH || "http://fe-host.rmu.local/",
+    publicPath: "http://fe-host.rmu.local/",
   },
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
@@ -83,9 +85,12 @@ module.exports = (_, argv) => ({
       name: "host",
       filename: "remoteEntry.js",
       remotes: {
-        strategic: process.env.RMU_MODULE_FEDERATION_STRATEGIC || "strategic@http://localhost:8082/strategic-app.js",
-        tactical: process.env.RMU_MODULE_FEDERATION_TACTICAL || "tactical@http://localhost:8083/tactical-app.js",
-        npc: process.env.RMU_MODULE_FEDERATION_NPC || "npc@http://localhost:8084/npc-app.js",
+        // strategic: process.env.RMU_MODULE_FEDERATION_STRATEGIC || "strategic@http://localhost:8082/strategic-app.js",
+        // tactical: process.env.RMU_MODULE_FEDERATION_TACTICAL || "tactical@http://localhost:8083/tactical-app.js",
+        // npc: process.env.RMU_MODULE_FEDERATION_NPC || "npc@http://localhost:8084/npc-app.js",
+        strategic: "strategic@http://fe-strategic.rmu.local/strategic-app.js",
+        tactical: "tactical@http://fe-tactical.rmu.local/tactical-app.js",
+        npc: "npc@http://fe-npc.rmu.local/npc-app.js",
       },
       exposes: {
         './theme': './src/theme'
