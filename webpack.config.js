baa027f74e0fd81a4f2780e9f9a35dc834df5194
labcabrera <lab.cabrera@gmail.com>
2025-08-22 17:45:05 +0200

    Updated versiones. Fix proxy.

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,28 @@ module.exports = (_, argv) => ({
     port: 8080,
     historyApiFallback: true,
     watchFiles: [path.resolve(__dirname, "src")],
+    proxy: [
+      {
+        context: ['/dev/api/strategic'],
+        target: 'http://localhost:3002',
+        changeOrigin: true,
+        pathRewrite: { '^/dev/api/strategic': '' },
+        onProxyReq: function(proxyReq, req, res) {
+          console.log('[proxy strategic] Redirect:', req.url, '->', proxyReq.getHeader('host'));
+          proxyReq.setHeader('Authorization', 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIxSEpkcmJ4ZFpVVVI2YnBNemxZYV95LWFsMUZLY21iYlpfbGVoTFl0cUVFIn0.eyJleHAiOjE3NTU5NTQzMDgsImlhdCI6MTc1NTg2NzkwOCwianRpIjoib25ydHJvOjM2ZDgyNzIwLTU4NTQtNmY0NC0zMzQ1LTQxYjY4Yjk2NWVjYiIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA5MC9yZWFsbXMvcm11LWxvY2FsIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjQ4YmM4YTllLWJhNjAtNDAwZi1iYmY0LWVhYWI0OWJjN2YzYSIsInR5cCI6IkJlYXJlciIsImF6cCI6InJtdS1jbGllbnQiLCJzaWQiOiIxYWNmZmFjYy0zNzc2LTRhYTgtYWU3My0wZGE3OTZiZjBhNjciLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbIioiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwiYWRtaW4iLCJ1bWFfYXV0aG9yaXphdGlvbiIsImZhY3Rpb24tbWFuYWdlbWVudCIsImRlZmF1bHQtcm9sZXMtcm11LWxvY2FsIiwicm11LWFkbWluIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5hbWUiOiJMdWlzIENhYnJlcmEiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXQiLCJnaXZlbl9uYW1lIjoiTHVpcyIsImZhbWlseV9uYW1lIjoiQ2FicmVyYSIsImVtYWlsIjoibGFiLmNhYnJlcmFAZ21haWwuY29tIn0.tnRPHLbTg3s6SEPyM5nqMBWRx_v82wodSQtX17Rj9XTdWiHhOilN-wOwxa83j0dP04N04weo0JP8jpjW14ZoDsyJlOjmZCxwU0vSHH1MSTF9FvAtYrjivCgJKNBDdDPmP5bBFeMopmYe5Z-Jbtx-pja_nAsnMLqeH1Im2--StVUdEMH639JSOJ73PwnjjRabU7s_t50id07PF0WL_zQ2oLIJJtdt6Hpc5ZK9thqV7NqZqFMwkox_dIZB58gg-xky-oeLWc7_O0Vs5aP5AcLcxHUZplcu0RNkC4HYhs5kcLXsrSbi1crRSKb7jgD41h7styDang0f7i0aojhUmeNXAA');
+        }
+      },
+      {
+        context: ['/dev/api/core'],
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        pathRewrite: { '^/dev/api/core': '' },
+        onProxyReq: function(proxyReq, req, res) {
+          console.log('[proxy core] Redirect:', req.url, '->', proxyReq.getHeader('host'));
+          proxyReq.setHeader('Authorization', 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIxSEpkcmJ4ZFpVVVI2YnBNemxZYV95LWFsMUZLY21iYlpfbGVoTFl0cUVFIn0.eyJleHAiOjE3NTU5NTQzMDgsImlhdCI6MTc1NTg2NzkwOCwianRpIjoib25ydHJvOjM2ZDgyNzIwLTU4NTQtNmY0NC0zMzQ1LTQxYjY4Yjk2NWVjYiIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA5MC9yZWFsbXMvcm11LWxvY2FsIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjQ4YmM4YTllLWJhNjAtNDAwZi1iYmY0LWVhYWI0OWJjN2YzYSIsInR5cCI6IkJlYXJlciIsImF6cCI6InJtdS1jbGllbnQiLCJzaWQiOiIxYWNmZmFjYy0zNzc2LTRhYTgtYWU3My0wZGE3OTZiZjBhNjciLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbIioiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwiYWRtaW4iLCJ1bWFfYXV0aG9yaXphdGlvbiIsImZhY3Rpb24tbWFuYWdlbWVudCIsImRlZmF1bHQtcm9sZXMtcm11LWxvY2FsIiwicm11LWFkbWluIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5hbWUiOiJMdWlzIENhYnJlcmEiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXQiLCJnaXZlbl9uYW1lIjoiTHVpcyIsImZhbWlseV9uYW1lIjoiQ2FicmVyYSIsImVtYWlsIjoibGFiLmNhYnJlcmFAZ21haWwuY29tIn0.tnRPHLbTg3s6SEPyM5nqMBWRx_v82wodSQtX17Rj9XTdWiHhOilN-wOwxa83j0dP04N04weo0JP8jpjW14ZoDsyJlOjmZCxwU0vSHH1MSTF9FvAtYrjivCgJKNBDdDPmP5bBFeMopmYe5Z-Jbtx-pja_nAsnMLqeH1Im2--StVUdEMH639JSOJ73PwnjjRabU7s_t50id07PF0WL_zQ2oLIJJtdt6Hpc5ZK9thqV7NqZqFMwkox_dIZB58gg-xky-oeLWc7_O0Vs5aP5AcLcxHUZplcu0RNkC4HYhs5kcLXsrSbi1crRSKb7jgD41h7styDang0f7i0aojhUmeNXAA');
+        }
+      }
+    ],
     onListening: function (devServer) {
       const port = devServer.server.address().port;
       printCompilationMessage("compiling", port);
@@ -102,14 +124,14 @@ module.exports = (_, argv) => ({
       exposes: {
       },
       shared: {
-        'react': { singleton: true, requiredVersion: '^19.1.1' },
-        'react-dom': { singleton: true, requiredVersion: '^19.1.1' },
-        'react-router-dom': { singleton: true, requiredVersion: '^7.7.0' },
-        'keycloak-js': { singleton: true, requiredVersion: '^26.2.0' },
-        '@mui/material': { singleton: true, requiredVersion: false },
-        '@mui/icons-material': { singleton: true, requiredVersion: false },
-        '@emotion/react': { singleton: true, requiredVersion: false },
-        '@emotion/styled': { singleton: true, requiredVersion: false },
+        react: { singleton: true, requiredVersion: deps.react },
+        'react-dom': { singleton: true, requiredVersion: deps["react-dom"] },
+        'react-router-dom': { singleton: true, requiredVersion: deps["react-router-dom"] },
+        '@mui/material': { singleton: true, requiredVersion: deps["@mui/material"] },
+        '@mui/icons-material': { singleton: true, requiredVersion: deps["@mui/icons-material"] },
+        '@emotion/react': { singleton: true, requiredVersion: deps["@emotion/react"] },
+        '@emotion/styled': { singleton: true, requiredVersion: deps["@emotion/styled"] },
+
       },
     }),
     new HtmlWebPackPlugin({
