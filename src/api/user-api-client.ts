c44008390b6b4eea85827697e42ca282b11f1ec7
labcabrera <lab.cabrera@gmail.com>
2026-05-16 16:37:03 +0200

    Updated user api.

--- src/api/user-api-client.ts
+++ src/api/user-api-client.ts
@@ -1,6 +1,16 @@
 import { AuthContextProps } from 'react-oidc-context';
 import { userApiBaseUrl } from '../services/config';
 
+export type Page<T> = {
+  content: T[];
+  pagination: Pagination;
+};
+
+export type Pagination = {
+  page: number;
+  pageSize: number;
+};
+
 export type ApiUser = {
   id: string;
   name: string;
@@ -10,6 +20,16 @@ export type ActivationCode = {
   id: string;
   code: string;
   features: string[];
+};
+
+export type Friendship = {
+  id: string;
+  requesterId: string;
+  addresseeName: string;
+  status: string;
+  message: string;
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 export class ApiError extends Error {
@@ -32,6 +52,55 @@ export const userApiClient = {
       return await response.json();
     }
     throw new ApiError(`Failed to fetch user: ${response.statusText}`, response.status);
+  },
+
+  async fetchFriends(auth: AuthContextProps): Promise<Page<Friendship>> {
+    const userId = auth.user?.profile.sub;
+    const rsql = `(requesterId==${userId},addresseeId==${userId});status==accepted)`;
+    const response = await fetch(`${userApiBaseUrl}/activation-codes/friendships`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${auth.user?.access_token}`,
+      },
+    });
+    if (response.ok) {
+      return await response.json();
+    }
+    const err = await response.json();
+    throw new ApiError(err.message || `Failed to check activation code: ${response.statusText}`, response.status);
+  },
+
+  async fecthInvitationsPending(auth: AuthContextProps): Promise<Page<Friendship>> {
+    const userId = auth.user?.profile.sub;
+    const rsql = `(addresseeId==${userId});status==pending)`;
+    //TODO page and size
+    const response = await fetch(`${userApiBaseUrl}/friendships?q=${rsql}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${auth.user?.access_token}`,
+      },
+    });
+    if (response.ok) {
+      return await response.json();
+    }
+    const err = await response.json();
+    throw new ApiError(err.message || `Failed to check activation code: ${response.statusText}`, response.status);
+  },
+
+  async fecthInvitationsSent(auth: AuthContextProps): Promise<Page<Friendship>> {
+    const userId = auth.user?.profile.sub;
+    const rsql = `(requesterId==${userId});status==pending)`;
+    const response = await fetch(`${userApiBaseUrl}/friendships?q=${rsql}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${auth.user?.access_token}`,
+      },
+    });
+    if (response.ok) {
+      return await response.json();
+    }
+    const err = await response.json();
+    throw new ApiError(err.message || `Failed to check activation code: ${response.statusText}`, response.status);
   },
 
   async fetchActivateCode(code: string, auth: AuthContextProps): Promise<ActivationCode> {
